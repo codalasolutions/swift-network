e094d154877a3529ca4c155b7303d1cbed35914b
Giorgi Kratsashvili <gkrat17@freeuni.edu.ge>
2022-10-17 20:05:07 +0400

    first commit

---
+++ Package.swift
@@ -0,0 +1,21 @@
+// swift-tools-version: 5.6
+
+import PackageDescription
+
+let package = Package(
+    name: "Network",
+    products: [
+        .library(
+            name: "Network",
+            targets: ["Network"]),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "Network",
+            dependencies: []),
+        .testTarget(
+            name: "NetworkTests",
+            dependencies: ["Network"]),
+    ]
+)
---
+++ Tests/NetworkTests/NetworkTests.swift
@@ -0,0 +1,8 @@
+import XCTest
+@testable import Network
+
+final class NetworkTests: XCTestCase {
+    func testExample() throws {
+        XCTAssertEqual(Network().text, "Hello, World!")
+    }
+}
