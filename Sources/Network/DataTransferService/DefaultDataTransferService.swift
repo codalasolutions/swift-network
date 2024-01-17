27ca220dd45edda852b9849918843222c63b348a
Giorgi Kratsashvili <gkrat17@freeuni.edu.ge>
2024-01-17 13:58:58 +0400

    declare wrappers as public only

--- Sources/Network/DataTransferService/DefaultDataTransferService.swift
+++ Sources/Network/DataTransferService/DefaultDataTransferService.swift
@@ -16,12 +16,12 @@ open class DefaultDataTransferService: DataTransferService {
     }
 
     @available(macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 13.0, *)
-    open func request<T: Decodable>(with request: URLRequest) async throws -> T {
+    public func request<T: Decodable>(with request: URLRequest) async throws -> T {
         try await self.request(with: request).data
     }
 
     @available(macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 13.0, *)
-    open func request<T: Decodable>(with request: URLRequest) async throws -> Response<T> {
+    public func request<T: Decodable>(with request: URLRequest) async throws -> Response<T> {
         try await withCheckedThrowingContinuation { continuation in
             self.request(with: request) { (result: Result<Response<T>, Error>) in
                 switch result {
@@ -35,12 +35,12 @@ open class DefaultDataTransferService: DataTransferService {
     }
 
     @available(macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 13.0, *)
-    open func request(with request: URLRequest) async throws -> Data {
+    public func request(with request: URLRequest) async throws -> Data {
         try await self.request(with: request).data
     }
 
     @available(macOS 10.15, iOS 13.0, watchOS 6.0, tvOS 13.0, *)
-    open func request(with request: URLRequest) async throws -> Response<Data> {
+    public func request(with request: URLRequest) async throws -> Response<Data> {
         try await withCheckedThrowingContinuation { continuation in
             self.request(with: request) { (result: Result<Response<Data>, Error>) in
                 switch result {
@@ -53,7 +53,7 @@ open class DefaultDataTransferService: DataTransferService {
         }
     }
 
-    open func request<T: Decodable>(with request: URLRequest, handler: @escaping (Result<T, Error>) -> Void) {
+    public func request<T: Decodable>(with request: URLRequest, handler: @escaping (Result<T, Error>) -> Void) {
         self.request(with: request) { (result: Result<Response<T>, Error>) in
             switch result {
             case .success(let response):
@@ -64,7 +64,7 @@ open class DefaultDataTransferService: DataTransferService {
         }
     }
 
-    open func request(with request: URLRequest, handler: @escaping (Result<Data, Error>) -> Void) {
+    public func request(with request: URLRequest, handler: @escaping (Result<Data, Error>) -> Void) {
         self.request(with: request) { (result: Result<Response<Data>, Error>) in
             switch result {
             case .success(let response):
